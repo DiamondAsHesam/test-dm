7c1fc67eb9ca2bc975463a4be9c2356a3a40ac47
DiamondAsHesam <99635385+DiamondAsHesam@users.noreply.github.com>
2025-06-03 22:29:30 +0330

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    document.getElementById("loading").style.display = "none";
-    document.getElementById("main-content").style.display = "block";
-  }, 15000); // ⏱️ تغییر این عدد = تغییر زمان لودینگ (میلی‌ثانیه)
-});
