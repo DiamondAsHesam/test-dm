61127f958a116048eb722be7a99fe240d15ba80e
DiamondAsHesam <99635385+DiamondAsHesam@users.noreply.github.com>
2025-06-03 19:27:29 +0330

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+  }, 2000);
+});
