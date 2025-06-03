5cce244d3175d09fa4c2e517e7119a022f9f0fc0
DiamondAsHesam <99635385+DiamondAsHesam@users.noreply.github.com>
2025-06-03 19:42:00 +0330

    script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ window.addEventListener("load", () => {
   setTimeout(() => {
     document.getElementById("loading").style.display = "none";
     document.getElementById("main-content").style.display = "block";
-  }, 2000);
+  }, 15000); // ⏱️ تغییر این عدد = تغییر زمان لودینگ (میلی‌ثانیه)
 });
