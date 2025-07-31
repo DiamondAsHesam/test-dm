ba300af3a5b6d38e50c0d7eb87bc832dfc1f8339
DiamondAsHesam <99635385+DiamondAsHesam@users.noreply.github.com>
2025-07-31 00:40:02 -0700

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// انتخاب عناصر
 const body = document.body;
 const checkbox = document.getElementById("theme-toggle-checkbox");
 
+// بررسی تم ذخیره‌شده
 const savedTheme = localStorage.getItem("theme");
 if (savedTheme === "dark") {
     body.classList.add("dark-mode");
     checkbox.checked = true;
 }
 
+// تغییر تم
 checkbox.addEventListener("change", () => {
     if (checkbox.checked) {
         body.classList.add("dark-mode");
